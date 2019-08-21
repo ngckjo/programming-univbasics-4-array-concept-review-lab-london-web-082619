2d38348c72cbf990e49caad3ce28fe49fbd9c98c
Josh <cheukhinjoshua.ng@gmail.com>
2019-08-21 09:40:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,6 +4,8 @@ def find_element_index(array, value_to_find)
   while counter < array.length do
     if array(counter) == value_to_find
       puts counter
+    else
+      counter += 1
   end
 end
 
